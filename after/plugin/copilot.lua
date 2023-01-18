696c94d9928d3d3976748581f784350f4a1a92dc
ValValu <tomruery@gmail.com>
2023-01-18 18:14:25 +1300

    current vers

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,1 +1,16 @@
 vim.g.copilot_no_tab_map = true
+vim.keymap.set(
+    "i",
+    "<Plug>(vimrc:copilot-dummy-map)",
+    'copilot#Accept("")',
+    { silent = true, expr = true, desc = "Copilot dummy accept" }
+)
+
+local cmp = require("cmp")
+cmp.setup {
+  mapping = {
+    ['<M-a>'] = cmp.mapping(function(fallback)
+      vim.api.nvim_feedkeys(vim.fn['copilot#Accept'](vim.api.nvim_replace_termcodes('<Tab>', true, true, true)), 'n', true)
+    end)
+  },
+}
